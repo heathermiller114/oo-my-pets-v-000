b3be948b1757e869de726e2ba4881f24adfeca63
Heather Miller <heather.miller114@gmail.com>
2018-09-25 16:34:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  
+  attr_reader :species
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+  end
+  
 end
